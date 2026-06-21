6cc5d94de630bf7d0bbb44b8c4a444d27704e54a
himanshusalal <himanshusalal517@gmail.com>
2026-06-21 17:46:08 +0530

    frontend ready for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,19 @@
+// const API_URL =
+//   import.meta.env.VITE_API_URL ||
+//   "https://employee-management-system-3-316w.onrender.com";
+
+// export default API_URL;
+
+
+
+// const API_URL =
+//   import.meta.env.VITE_API_URL ||
+//   "http://localhost:5000";
+
+// export default API_URL;
+
+
+
 const API_URL =
   import.meta.env.VITE_API_URL ||
   "https://employee-management-system-3-316w.onrender.com";
