ecc037fe6873dfc843cee48a5cba5fc47de4ff42
himanshusalal <himanshusalal517@gmail.com>
2026-07-25 17:24:39 +0530

    Fix CORS issue

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,20 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://employee-management-system-daba.vercel.app"
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+
+
 app.use(express.json());
 
 // Routes
@@ -34,96 +47,3 @@ app.listen(PORT, () => {
 
 
 
-
-
-
-
-
-// const express = require("express");
-// const cors = require("cors");
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// let employees = [
-//   {
-//     id: 1,
-//     name: "Himanshu",
-//     department: "IT",
-//     salary: 50000
-//   },
-//   {
-//     id: 2,
-//     name: "Rahul",
-//     department: "HR",
-//     salary: 40000
-//   }
-// ];
-
-// app.get("/", (req, res) => {
-//   res.send("Employee API is Running...");
-// });
-
-
-// app.get("/employees", (req, res) => {
-//   res.json(employees);
-// });
-// // GET
-
-
-
-// // POST
-// app.post("/employees", (req, res) => {
-//   const employee = {
-//     id: Date.now(),
-//     ...req.body
-//   };
-
-//   employees.push(employee);
-
-//   res.json(employee);
-// });
-
-
-// // PUT
-
-// app.put("/employees/:id", (req, res) => {
-//   const id = Number(req.params.id);
-
-//   employees = employees.map(emp =>
-//     emp.id === id ? { ...emp, ...req.body } : emp
-//   );
-
-//   res.json({
-//     message: "Employee Updated"
-//   });
-// });
-
-
-// // DELETE
-
-// app.delete("/employees/:id", (req, res) => {
-//   const id = Number(req.params.id);
-
-//   employees = employees.filter(emp => emp.id !== id);
-
-//   res.json({
-//     message: "Employee Deleted"
-//   });
-// });
-
-
-// //const PORT = process.env.PORT || 5000;
-// // app.listen(PORT, () => {
-// //   console.log(`Server Running On Port ${PORT}`);
-// // });
-
-
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server Running On Port ${PORT}`);
-// });
