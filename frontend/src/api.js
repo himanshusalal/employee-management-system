46f0b65db613a0d8fc5a45a49b15da862058f0c6
himanshusalal <himanshusalal517@gmail.com>
2026-07-25 19:34:01 +0530

    Latest frontend update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 // Axios instance create
 const API = axios.create({
+
+    
 
     baseURL: import.meta.env.VITE_API_URL
 
